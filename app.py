c1919ea59f0d58cca1ca4dc6d1e3da4e2e1f9f1c
Revito <revitomanik@gmail.com>
2025-01-05 23:44:41 +0700

    2nd Commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Happy New Year 2025!"
+    return "Hello, Happy New Year 2025! Goodbye 2024!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
